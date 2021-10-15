e585f42987414841b8a33ecc2638cef7a07b0038
“Olha <kryvyo03@kits.kfplc.com>
2021-10-15 22:50:14 +0300

    first Task

---
+++ src/main/java/task1/DictionaryOperation.java
@@ -0,0 +1,21 @@
+package task1;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DictionaryOperation {
+
+    public Map<Character, Integer> characterMap(String text) {
+        char[] chars = text.toCharArray();
+        Map<Character, Integer> charsMap = new TreeMap<>();
+        for (int i = 0; i < chars.length; i++) {
+            if (charsMap.containsKey(chars[i])) {
+                Integer count = charsMap.get(chars[i]);
+                charsMap.put(chars[i], count + 1);
+            } else {
+                charsMap.put(chars[i], 1);
+            }
+        }
+        return charsMap;
+    }
+}
---
+++ src/main/java/task1/Main.java
@@ -0,0 +1,10 @@
+package task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DictionaryOperation dictionaryOperation = new DictionaryOperation();
+        System.out.println(dictionaryOperation.characterMap("djdjjdsbfhskaoaokiaso"));
+
+    }
+}
