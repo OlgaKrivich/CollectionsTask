0ed16ded5a35352291faa3d61ce95fe193eda6a9
“Olha <kryvyo03@kits.kfplc.com>
2021-10-17 17:55:15 +0300

    tasks

--- src/main/java/task5/CombinationOperation.java
+++ src/main/java/task5/CombinationOperation.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-public class CombineTwoListsIntoMapOperation {
+public class CombinationOperation {
 
     public ArrayList<String> addToKeyList() {
         ArrayList<String> listOfKeys = new ArrayList<>();
--- src/main/java/task5/Main.java
+++ src/main/java/task5/Main.java
@@ -3,7 +3,7 @@ package task5;
 public class Main {
 
     public static void main(String[] args) {
-        CombineTwoListsIntoMapOperation mapOperation = new CombineTwoListsIntoMapOperation();
+        CombinationOperation mapOperation = new CombinationOperation();
         mapOperation.combineTwoListsIntoMapVersion1(mapOperation.addToKeyList(),
                 mapOperation.addToValueList());
         mapOperation.combineTwoListsIntoMapVersion2(mapOperation.addToKeyList(),
